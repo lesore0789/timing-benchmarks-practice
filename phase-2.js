783d4e04cfaf45ae078c874c90f0c5f7cc8be582
Leslie <lesore0789@gmail.com>
2023-02-14 16:25:50 -0800

    Completed Phase 2

--- phase-2.js
+++ phase-2.js
@@ -2,14 +2,20 @@ const [addNums, addManyNums] = require("./phase-1");
 
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
-  // Fill this in
-
+  let total = [];
+  for(let i = increment; i <= 10 * increment; i += increment){
+    total.push(addNums(i))
+  }
+  return total;
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
-  // Fill this in
-
+  let total = [];
+  for(let i = increment; i <= 10 * increment; i += increment){
+    total.push(addManyNums(i))
+  }
+  return total
 }
 
 module.exports = [addNums10, addManyNums10];
