ba30f40570feb0ad8db5109961e148b1531b6b8e
Leslie <lesore0789@gmail.com>
2023-02-14 16:36:26 -0800

    Completed Phase 3

--- phase-3.js
+++ phase-3.js
@@ -1,19 +1,41 @@
 const [addNums, addManyNums] = require("./phase-1");
 
 function addNums10Timing(increment) {
-  // Copy your `addNums10` code here
-  // Then, add timing code
+  let total = [];
+  for(let i = increment; i <= 10 * increment; i += increment){
 
-  // Your code here
+    console.time(`Timer for increment ${i}`);
+
+    let startTime = Date.now();
+    let result = addNums(i);
+    let endTime = Date.now();
+
+    console.timeEnd(`Timer for increment ${i}`);
+    console.log(`Runtime: ${endTime - startTime}ms`);
+
+    total.push(result)
+  }
+  return total;
 
 }
 
 
 function addManyNums10Timing(increment) {
-// Copy your `addManyNums10` code here
-// Then, add timing code
+  let total = [];
+  for(let i = increment; i <= 10 * increment; i += increment){
 
-  // Your code here
+    console.time(`Timer for increment ${i}`);
+
+    let startTime = Date.now();
+    let result = addManyNums(i);
+    let endTime = Date.now();
+
+    console.timeEnd(`Timer for increment ${i}`);
+    console.log(`Runtime: ${endTime - startTime}ms`);
+
+    total.push(result);
+  }
+  return total
 
 }
 
